deff6b37dcc144cbcde8037b6f47b2c367ecbc2b
Dimitry Declercq <mail@dimy.be>
2016-07-26 19:12:23 +0200

    Adding example Slack implementation

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ Pre-Requisites:
 - CLI experience
 - Some sort of programming experience
 - Node.js
-- NPM
+- NPM`
+- Slack, with a incoming webhook
 
 Getting things up and running
 
 Install dependencies
 > npm install
 
-Run this server
-> npm run server
+Run this server (only osx/linux)
+> slackUri=your-slack-webhook-url npm run server
+For windows: set slackUri as an environment variable and run npm run server`
 
 Start PokemonGo-Maps (see https://github.com/AHAAAAAAA/PokemonGo-Map)
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
 
-startServer(3000);
+startServer(process.env.port || 3000);
 
 function startServer(port) {
   // Configure some logging so we can pretend to know whats going on
   var Logger = require('bunyan');
   var logger = new Logger({
-    name: "My Custom PoGo-Maps Extension",
+    name: "Pokemon Slack Extension",
     src: false,
     streams: [{
       // let's just write to the trusty STDOUT
@@ -22,10 +22,11 @@ function startServer(port) {
   });
 
   // Get our API up so it can be called from PoGo-Maps
-  var webhookApi = require('./lib/api')(logger);
+  var slackService = require('./lib/slack')(logger);
+  var webhookApi = require('./lib/api')(slackService, logger);
   webhookApi.listen(port, function (err) {
     if (err) throw err;
-    logger.info('My Custom PoGo-Maps Extension listening on port ' + port);
+    logger.info('Pokemon Slack Extension listening on port ' + port);
   });
 }
 
